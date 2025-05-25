26086967f90a57821fb0dbc7a1b1b6402efb5fc0
Ayush Singh <ayushsinghbharti123@gmail.com>
2025-05-25 19:28:05 +0530

    init project

---
+++ components/colours.tsx
@@ -0,0 +1,10 @@
+const colours = {
+    primaryBackground: '#F9F9F9', // White
+    primaryButton: '#B5B25C', // Olive Gold
+    secondaryBackground: '#EAE0C8', // Pearl
+    secondaryButton: '#536878', // Grey
+    error: '#FC350B', // Red
+    errorBackground: '#FEF1E1', // Peach
+};
+
+export default colours;
